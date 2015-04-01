3c9f62a896dcfe7d70605a0719aba06563e48303
Smith, Cooper <cooper.smith@jwt.com>
2015-04-01 18:11:59 -0400

    finally got superselector working, from here on in use devleop branch

--- app.js
+++ app.js
@@ -39,84 +39,60 @@ var ctgArr =[ {"id":"0", "cat": "Please select category, untill all categories a
 				{"id":"4","cat": "Finance"},
 				{"id":"5","cat":"Food"}];
 
-var ctgObj = { 
+/*var ctgObj = { 
       id: $scope.catId,
       cat: $scope.category 
-      } 
-
-  /*    $scope.campaign.title,
-      description: $scope.campaign.description,
-      launch: $scope.launchDate,
-      end: $scope.drawingDate,
-      country: $scope.campaign.country,
-      image: $scope.campaign.image,
-      video: $scope.campaign.video,
-      clicks: 0
-    }  */
+      } */
 
  console.log("Set up categories: ", ctgArr);
 
    var options = ctgArr;
     // make sure the selector get the final options
        $scope.options = options;
-       //$scope.selectedOption = $scope.options[0];
+       $scope.selectedOption = $scope.options[0];
        var first = false; 
 
  $scope.$watch('selectedOption', function(newValue, oldValue) {
                     // handle selection change ...
-
-          var ctgObj = { 
-            id: "myId",
-            cat: $scope.options[0].cat  
-            } 
-
-
-          //ctgObj = $scope.selectedOption;
-
-                    	console.log ("My expert Controller is in control! " , ctgObj.cat );
-
                     var selectObj;
                     selectObj = $scope.selectedOption;
 
-                    console.log("expertController - selection: " , oldValue );
+                    console.log("expertController - selection: " , $scope.selectedOption.id );
                     var newOptions = new Array(); 
                     var option = $scope.selectedOption;
-                    //optionNo = $scope.selectedOption.id;
-
-                    console.log("choice number: " + optionNo+ " option selected: ", $scope.options[optionNo].cat );
+                  
                     console.log("current options: " +  options );
-
-                    expertData[optionNo] = option;
-
-                   //reset option array to exclude selection / add selection to profile array FIFO  
-                     for ( var i=0 ; i <  $scope.options.length ; i++) {
-                        if ( $scope.options[i].cat != $scope.options[optionNo].cat ) {
-                           console.log("ExpertController - comparing: " + $scope.options[i].cat + " with current selected option:  ", $scope.options[optionNo].cat );
-                            newOptions.push($scope.options[i]);
-                        } //end if          
-                    }//end for loop..
                     ++optionNo;
-                    console.log("new options list: " , newOptions );
-                    $scope.options = newOptions;
-                    $scope.selectedOption = $scope.options[0];
 
-                    //$scope.options = options;
-                	first = true;
-                } );//end watch selectedOption 
+                    if ( (optionNo != 0) && ($scope.selectedOption.id != 0 )) {
+                      for ( var i=0 ; i <  $scope.options.length ; i++) {
+                        if ( $scope.options[i].cat != $scope.selectedOption.cat ) {
+                           newOptions.push($scope.options[i]);
+                        } else {
+                            console.log("add to expert array: " + $scope.options[i].cat);
+                            //expertData[optionNo] = { id: optionNo , cat: $scope.options[i].cat };
+                            expertData.push({id: optionNo, cat: $scope.options[i].cat });
+                        }//end if          
+                      } //end for
+  
+                      $scope.options = newOptions;
+                      $scope.selectedOption = newOptions[0];
+                      //++optionNo;
+                    }//end iff 
+                   
+                });//end watch selectedOption 
 
 
  $scope.showProfile = function () {
- /*  	 console.log("getProfile - Build our user profile: " , expertData[1].cat );
+  	 console.log("getProfile - Build our user profile: " , expertData );
 
    	 var showArr = new Array();
-
-   	 for (var i=1 ;i < expertData.length ; i++) {
-   	 	console.log("preference: " , expertData[i] );
-   	 	showArr[i] = expertData[i].cat+" ";
-   	 }
+     for ( var i=0; i < expertData.length ; ++i ) {
+        showArr[i] = expertData[i].cat;
+     }
 
    	 alert("Here is your profile preferences: " + showArr );
-   	 alert("Get ready for those emails!!!"); */
+   	 alert("Get ready for those emails!!!"); 
   }//end getData
 
 });//end controller
